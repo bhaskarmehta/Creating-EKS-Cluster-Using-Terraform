502ef68b1f2b872a8d079253e09a280cdccccd03
bhaskarmehta <46514777+bhaskarmehta@users.noreply.github.com>
2023-09-21 09:15:51 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -57,3 +57,23 @@ resource "aws_eks_node_group" "demo" {
     aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+resource "aws_eks_node_group" "demo-medium" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "demo-medium"
+  node_role_arn   = aws_iam_role.demo-node.arn
+  subnet_ids      = aws_subnet.demo[*].id
+  instance_types = ["t2.mmedium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.demo-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.demo-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
